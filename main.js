ccac64026282e9a2b1edc0b1d5c9924e4654c66f
Cayo デカア <144740711+cayodka@users.noreply.github.com>
2024-10-04 21:39:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,22 @@ let BotaoAdd =  document.getElementById('buttonAdd');
 let Input = document.getElementById('inputTarefas')
 let Tarefas = document.getElementById('tarefas')
 let marcado = false;
+ const dataAtual = new Date().toISOString().split('T')[0];
+ const sino1 = new Audio('./sino1.mp3')
+const sino2 = new Audio('./sino2.mp3')
 
 BotaoAdd.addEventListener('click', function(){
+   
     if(Input.value !=''){
         var tarefa = document.createElement('p');
+        tarefa.innerText = `${dataAtual} | ${Input.value}`;
         Tarefas.appendChild(tarefa);
-        tarefa.innerText = '- ' + Input.value;
         Input.value = '';
         tarefa.addEventListener('click', function(){
             if (marcado == false) {
                 tarefa.style.textDecoration = 'line-through';
                 marcado = true;
+                sino1.play();
             }
             else{
                 tarefa.style.textDecoration = '';
@@ -21,6 +26,7 @@ BotaoAdd.addEventListener('click', function(){
         })
         tarefa.addEventListener('dblclick', function(){
             Tarefas.removeChild(tarefa);
+            sino2.play();
         })
     }
 })
