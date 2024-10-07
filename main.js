8f798576e06beb5972fb9d3b0eda9453529a46c1
Cayo デカア <144740711+cayodka@users.noreply.github.com>
2024-10-07 14:58:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-let BotaoAdd =  document.getElementById('buttonAdd');
-let Input = document.getElementById('inputTarefas')
-let Tarefas = document.getElementById('tarefas')
-let marcado = false;
- const dataAtual = new Date().toISOString().split('T')[0];
- const sino1 = new Audio('./sino1.mp3')
-const sino2 = new Audio('./sino2.mp3')
+let BotaoAdd = document.getElementById('buttonAdd');
+let Input = document.getElementById('inputTarefas');
+let Tarefas = document.getElementById('tarefas');
+const dataAtual = new Date().toISOString().split('T')[0];
+const sino1 = new Audio('./sino1.mp3');
+const sino2 = new Audio('./sino2.mp3');
 
-BotaoAdd.addEventListener('click', function(){
-   
-    if(Input.value !=''){
+BotaoAdd.addEventListener('click', function() {
+    if (Input.value != '') {
         var tarefa = document.createElement('p');
-        tarefa.innerText = `${dataAtual} | ${Input.value}`;
+        var buttondel = document.createElement('button');
+        buttondel.innerText = 'X'; 
+        buttondel.setAttribute("id", "buttondel");
+        
+        tarefa.innerText = `${dataAtual} | ${Input.value} `;
+        tarefa.appendChild(buttondel); 
         Tarefas.appendChild(tarefa);
         Input.value = '';
-        tarefa.addEventListener('click', function(){
-            if (marcado == false) {
+
+        let marcado = false;
+
+        tarefa.addEventListener('click', function() {
+            if (!marcado) {
                 tarefa.style.textDecoration = 'line-through';
                 marcado = true;
                 sino1.play();
-            }
-            else{
+            } else {
                 tarefa.style.textDecoration = '';
                 marcado = false;
             }
-        })
-        tarefa.addEventListener('dblclick', function(){
+        });
+
+        buttondel.addEventListener('click', function() {
             Tarefas.removeChild(tarefa);
             sino2.play();
-        })
+        });
     }
-})
+});
